bf5184bea9e58a35e9959b207b6f426ea56d5211
Ekaterina <kateshenyang@gmail.com>
2024-09-05 18:17:25 +0300

    fixed by prettier

--- src/js/cardType.js
+++ src/js/cardType.js
@@ -1,15 +1,15 @@
 export function getCardType(number) {
-    const visaRegex = new RegExp('^4[0-9]{12}(?:[0-9]{3})?$');
-    const mastercardRegex = new RegExp('^5[1-5][0-9]{14}$');
-    const mirRegex = new RegExp('^220[0-4][0-9]{12}$');
-  
-    if (visaRegex.test(number)) {
-      return 'Visa';
-    } else if (mastercardRegex.test(number)) {
-      return 'Mastercard';
-    } else if (mirRegex.test(number)) {
-      return 'Mir';
-    } else {
-      return 'Unknown';
-    }
+  const visaRegex = new RegExp('^4[0-9]{12}(?:[0-9]{3})?$');
+  const mastercardRegex = new RegExp('^5[1-5][0-9]{14}$');
+  const mirRegex = new RegExp('^220[0-4][0-9]{12}$');
+
+  if (visaRegex.test(number)) {
+    return 'Visa';
+  } else if (mastercardRegex.test(number)) {
+    return 'Mastercard';
+  } else if (mirRegex.test(number)) {
+    return 'Mir';
+  } else {
+    return 'Unknown';
   }
+}
--- src/js/validator.js
+++ src/js/validator.js
@@ -1,15 +1,15 @@
 export function validateCardNumber(number) {
-    const regex = new RegExp('^[0-9]{16}$');
-    if (!regex.test(number)) return false;
-  
-    let sum = 0;
-    for (let i = 0; i < number.length; i++) {
-      let intVal = parseInt(number.substr(i, 1));
-      if (i % 2 === 0) {
-        intVal *= 2;
-        if (intVal > 9) intVal = 1 + (intVal % 10);
-      }
-      sum += intVal;
+  const regex = new RegExp('^[0-9]{16}$');
+  if (!regex.test(number)) return false;
+
+  let sum = 0;
+  for (let i = 0; i < number.length; i++) {
+    let intVal = parseInt(number.substr(i, 1));
+    if (i % 2 === 0) {
+      intVal *= 2;
+      if (intVal > 9) intVal = 1 + (intVal % 10);
     }
-    return sum % 10 === 0;
+    sum += intVal;
   }
+  return sum % 10 === 0;
+}
